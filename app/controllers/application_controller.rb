105844102fb11f972b008bd6184ab871e0ae1ef8
melodytng <melodytang2002@yahoo.com>
2018-07-20 18:29:09 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,33 @@
+# require_relative 'config/environment'
+
 class ApplicationController < Sinatra::Base
 
-  configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
+require 'sinatra'
+    configure do
+  	set :views, "app/views"
+  	set :public_folder, "public"
   end
-
+  
+  get '/' do
+    return erb :layout
+  end
+  
+  post '/' do
+    return erb :quiz 
+  end
+  
+  get '/pacific.erb' do
+    erb :pacific
+  end
+  
+  post '/' do 
+    a1 = params[:ans]
+    a2 = params[:ans2]
+    a3 = params[:ans3]
+    a4 = params[:ans4]
+    a5 = params[:ans5]
+    a6 = params[:ans6]
+    @footprint = quiz(a1, a2, a3, a4, a5, a6)
+    return erb :results
+  end  
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,19 @@
-class Question
 
+def quiz(yn, yn2, yn3, yn4, yn5, yn6)
+  answers = [yn, yn2, yn3, yn4, yn5, yn6]
+  count=0
+  answers.each do |answer|
+    if answer == "yes"
+      count+=1 
+    end
+  end
+  if count <= 2 
+    "HUGE! Earth's resources are quickly depleting..."
+  elsif count <=4 
+    "average, but you can always be more green."
+  elsif count >= 5 
+    "small! Go you! But you can never be TOO environmentally friendly!"
+  end 
 end
+
+puts quiz("yes", "no", "yes", "no", "yes", "yes")
